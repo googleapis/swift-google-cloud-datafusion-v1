1a811bc6796e2de1331eb08443c69e2d1584bcd8
Jeff Ching <chingor@google.com>
2026-09-22 02:35:16 +0000

    fix(gax): mark internal GAX symbols with @_spi(GoogleCloudInternal) (#1009)
    Update `librarian` from `v0.44.1-0.20260922001047-b14705360e44` to
    `v0.44.1-0.20260922004056-eda963358ecc`
    ([compare](https://github.com/googleapis/librarian/compare/b14705360e44...eda963358ecc))
    and mark internal underscore-prefixed declarations in `GoogleGax` with
    `@_spi(GoogleCloudInternal)`.
    
    ### Summary of Changes
    1. **Update `librarian` and regenerate client libraries (`902972638`)**:
    - Includes
    [googleapis/librarian#7635](https://github.com/googleapis/librarian/pull/7635):
    emits `@_spi(GoogleCloudInternal)` on generated
    `GoogleGax._PaginatedResponse` and
    `GoogleGax._AggregatedPaginatedResponse` extension conformances and
    `@_spi(GoogleCloudInternal) import` statements in generated
    client/protocol files.
    2. **Annotate internal `GoogleGax` symbols with
    `@_spi(GoogleCloudInternal)` (`7569ac705`)**:
    - Marks `_ApiClientHeader`, `_PollableOperationImpl`,
    `_PaginatedResponse`, `_PaginationResult`, `_PaginatedResponseResult`,
    `_PaginationRequest`, `_Paginator`, `_AsyncItemSequence`,
    `_AggregatedPaginatedResponse`, `_AggregatedPaginator`, and
    `_AggregatedAsyncItemSequence` (and their public members) with
    `@_spi(GoogleCloudInternal)` so they are hidden from standard IDE
    autocomplete and public documentation.
    
    Fixes #961
    Fixes #962

--- Sources/GoogleCloudDataFusionV1/ListAvailableVersionsResponse.swift
+++ Sources/GoogleCloudDataFusionV1/ListAvailableVersionsResponse.swift
@@ -20,7 +20,6 @@ import Foundation
 
 /// Response message for the list available versions request.
 public struct ListAvailableVersionsResponse: Codable, Equatable, GoogleWKT._AnyPackable,
-  GoogleGax._PaginatedResponse,
   Sendable
 {
   /// Represents a list of versions that are supported.
@@ -95,7 +94,10 @@ public struct ListAvailableVersionsResponse: Codable, Equatable, GoogleWKT._AnyP
   public func _pack() throws -> GoogleWKT.Struct {
     return try GoogleWKT._slowAnySerialize(message: self)
   }
+}
 
+@_spi(GoogleCloudInternal)
+extension ListAvailableVersionsResponse: GoogleGax._PaginatedResponse {
   public func _getPaginatedItems() -> [Version] {
     return self.availableVersions
   }
--- Sources/GoogleCloudDataFusionV1/ListInstancesResponse.swift
+++ Sources/GoogleCloudDataFusionV1/ListInstancesResponse.swift
@@ -20,7 +20,6 @@ import Foundation
 
 /// Response message for the list instance request.
 public struct ListInstancesResponse: Codable, Equatable, GoogleWKT._AnyPackable,
-  GoogleGax._PaginatedResponse,
   Sendable
 {
   /// Represents a list of Data Fusion instances.
@@ -104,7 +103,10 @@ public struct ListInstancesResponse: Codable, Equatable, GoogleWKT._AnyPackable,
   public func _pack() throws -> GoogleWKT.Struct {
     return try GoogleWKT._slowAnySerialize(message: self)
   }
+}
 
+@_spi(GoogleCloudInternal)
+extension ListInstancesResponse: GoogleGax._PaginatedResponse {
   public func _getPaginatedItems() -> [Instance] {
     return self.instances
   }
